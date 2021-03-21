31e780b9e3f5650eecaa418ccf4e8f9738e9a2ae
Oleg Ananyev <32748936+OlegAnanyev@users.noreply.github.com>
2021-03-21 16:44:40 +0300

    Update Welcomer.java

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -2,9 +2,12 @@ package plaindoll;
 
 public class Welcomer{
 	public String sayWelcome() {
-		return "Welcome home, goood hunter. What is it your desire?";
+		return "Welcome home, good hunter. What is it your desire?";
 	}
 	public String sayFarewell() {
-		return "Farewell, goood hunter. May you find your worth in waking world.";
+		return "Farewell, good hunter. May you find your worth in waking world.";
+	}
+	public String sayZiggurat() {
+		return "You need to build a ziggurat, good hunter!";
 	}
 }
